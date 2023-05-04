6f0ac468a95ef3c12dc6210db658ed6a9ede2cc8
Zaw Min Tun <75002871+zawmorntwan@users.noreply.github.com>
2023-05-04 21:50:31 +0630

    update expenses

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'expenses_list/expenses_list.dart';
+import '../models/expanse.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('Chart'),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../../models/expanse.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Text(expense.title),
+    );
+  }
+}
