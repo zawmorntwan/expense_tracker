ad0e260ef69dc8ed04f9aaed84c310f19d4f1e08
Zaw Min Tun <75002871+zawmorntwan@users.noreply.github.com>
2023-05-07 12:11:59 +0630

    Update color theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 import 'widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(
+    255,
+    96,
+    59,
+    181,
+  ),
+);
+
 void main() {
   runApp(
     MaterialApp(
       home: const Expenses(),
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer
+        ),
+      ),
       debugShowCheckedModeBanner: false,
     ),
   );
