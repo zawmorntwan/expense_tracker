ab13440b132b68591a13268880d4e696a3285d57
Zaw Min Tun <75002871+zawmorntwan@users.noreply.github.com>
2023-05-08 21:22:54 +0630

    update chart

--- lib/models/expanse.dart
+++ lib/models/expanse.dart
@@ -29,12 +29,35 @@ class Expense {
   }) : id = uuid.v4();
 
   final double amount;
+  final Category category;
   final DateTime date;
   final String id;
   final String title;
-  final Category category;
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'chart/chart.dart';
 import 'expenses_list/expenses_list.dart';
 import '../models/expanse.dart';
 import 'new_expense.dart';
@@ -91,7 +92,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('Chart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
       ),
